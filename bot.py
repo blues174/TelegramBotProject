75361bee95d3eddfa1fbcaf44e7eec18035da3b0
blues174 <mishasamarofff@yandex.ru>
2025-01-24 15:54:15 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "Привет! Я простой бот. Задайте мне вопрос.")
+    await bot.send_message(message.chat.id, "Привет! Я бот погоды.")
 
 @dp.message_handler()
 async def echo(message: types.Message):
@@ -20,20 +20,6 @@ async def echo(message: types.Message):
     user_message = message.text.lower()
     if 'привет' in user_message:
         await bot.send_message(message.chat.id, 'Привет!')
-    elif 'как дела' in user_message:
-        await bot.send_message(message.chat.id, 'У меня все хорошо, спасибо!')
-    elif 'что ты умеешь' in user_message:
-        await bot.send_message(message.chat.id, 'Я умею общаться с людьми и отвечать на вопросы.')
-    elif 'какой твой любимый цвет' in user_message:
-        await bot.send_message(message.chat.id, 'Хороший вопрос! Мой любимый цвет - зелёный.')
-    elif 'каких животных ты любишь' in user_message:
-        await bot.send_message(message.chat.id, 'Я очень люблю кошек.')
-    elif 'игрок' in user_message:
-        await bot.send_message(message.chat.id, 'Воооооооооооооот так ты игрок заявил.')
-    elif 'что ты весь бой делал' in user_message:
-        await bot.send_message(message.chat.id, 'Я базу дефал')
-    elif 'зачем ты базу дефал' in user_message:
-        await bot.send_message(message.chat.id, 'А у меня геймплей такой')
     else:
         await bot.send_message(message.chat.id, 'Извините, я не понимаю ваш вопрос.')
 
